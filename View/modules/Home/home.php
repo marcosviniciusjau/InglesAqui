01b704cf13142037a25b3a6369f52a04bd7e9fdc
marcosviniciusjau <marcosviniciusjahu@gmail.com>
2023-06-19 15:53:22 -0300

    final

--- View/modules/Home/home.php
+++ View/modules/Home/home.php
@@ -3,18 +3,17 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
     <title>Inglês Aqui</title>
     <link rel="icon" href="/View/Imagens/icon.png" type="image/icon type">
-    <link rel="stylesheet" href="\View\css\home.css">
+    <link rel="stylesheet" href="/View/css/home.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous"> 
   </head>
   <body>
 <main>
 
     <img src="/View/Imagens/fundo.png" width="100%" height="50%">
-     <div class="d-md-flex flex-md-equal  w-100" id="sobre1">
-       <img src="View/Imagens/sobre.png" id="sobre_img" width="50%" height="50%">
+     <div class="d-md-flex flex-md-equal  w-100" class="azul">
+       <img src="View/Imagens/sobre.png" class="imgs">
       
       <div id="sobre">
        <div class="my-5 py-5">
@@ -34,19 +33,22 @@
         <a href="/apostilas"><button class="botao" >Ver mais</button></a>
       </div>
      </div>
-        <span id="livros">
-        <a href="/apostilas_desc?id=1"><img id="apostilas" src="View/Imagens/livro1.jpg"  width="80%" height="80%" class="thumbnail" /></a>
+        <span class="livros">
+        <a href="/apostilas_desc?id=1"><img class="apostilas" src="View/Imagens/livro1.png"  class="thumbnail" /></a>
         </span>
-        <span id="livros">
-        <a href="/apostilas_desc?id=7"><img id="apostilas" src="View/Imagens/livro2.jpg"  width="80%" height="80%" class="thumbnail" /></a>
+        <span class="livros">
+        <a href="/apostilas_desc?id=7"><img class="apostilas"  src="View/Imagens/livro2.png"  class="thumbnail" /></a>
         </span>
-        <span id="livros"> 
-        <a href="/apostilas_desc?id=3"><img id="apostilas" src="View/Imagens/livro3.jpg"  width="80%" height="80%" class="thumbnail" /></a>
+        <span class="livros"> 
+        <a href="/apostilas_desc?id=3"><img class="apostilas"  src="View/Imagens/livro3.png"  class="thumbnail" /></a>
+        <br> <br>
+        <br> <br>
         </span>
+       
     </div>
 
-  <div class="d-md-flex flex-md-equal  " id="sobre1">
-      <img src="View\Imagens/mundo.png" id="mundo_img" width="50%" height="50%"/>
+  <div class="d-md-flex flex-md-equal" class="azul">
+      <img src="View\Imagens/mundo.png" id="mundo_img" class="imgs"/>
     <div  id="mensagem">
       <div class="my-4 py-4">
         <h2 class="display-4" id="titulo_mensagem">Aprender inglês pode te levar para onde você  quiser!</h2>     
@@ -54,20 +56,19 @@
     </div>
   </div>
 
-  <div class="d-md-flex flex-md-equal" id="contato">
-    <div  class="my-5 py-5" id="contato_img">
-      <label class="display-5" id="titulo_contato">CONTATOS:</label>
+  <div class="d-md-flex flex-md-equal  w-100" id="contato">
+    <div  class="my-4 py-4" id="contato_img">
+      <label class="display-4" id="titulo_contato">CONTATOS:</label>
       <p  id="rede_social">@teacherschay</p>
       <p class="lead" >Siga nas redes sociais e vem falar com a gente!</p>
       <br>
         <a href="/contato"><button class="botao">Ver mais</button></a>
    </div>
-      <img src="View\Imagens/contato.png" width="50%" height="50%">
+      <img src="View/Imagens/contato.png" id="contato_imagem" class="imgs">
   </div>
      
 </main>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
      
   </body>
 </html>
