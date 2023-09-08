992e82e4dd5a63b0a747228e4aae011147a17978
marcosviniciusjau <marcosviniciusjahu@gmail.com>
2023-09-08 16:58:04 -0300

    testes

--- bot.py
+++ bot.py
@@ -1,9 +1,26 @@
+import openai
 import os
-import pandas as pandas
-from transformers import GPT2TokenizerFast
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.vectorstores import FAISS
-from langchain.chains.question_answering import load_qa_chain
-from langchain.llms import OpenAI
-from langchain.chains import ConversationalRetrievalChain
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def open_file(filepath):
+    with open(filepath, 'r', encoding='utf-8') as infile:
+        return infile.read()
+
+def save_file(filepath, content):
+    with open(filepath, 'a', encoding='utf-8') as outfile:
+        outfile.write(content)
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+
+# Faça a chamada à API diretamente usando a biblioteca openai
+with open("../App/mydata.jsonl","rb") as file:
+    response = openai.File.create(
+    file=file,
+    purpose='fine-tune'
+)
+
+    file_id = response['id']
+    print(f"File uploaded successfully with ID: {file_id}")
