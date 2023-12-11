7cef93d6d1e801c9541806f72629384454f338b7
Marcos Vinicius Araujo da Silva <marcosviniciusjahu@gmail.com>
2023-12-11 18:58:19 -0300

    ajustes

--- email.php
+++ email.php
@@ -12,7 +12,7 @@ try {
     $mail->Host       = 'smtp.gmail.com';
     $mail->SMTPAuth   = true;
     $mail->Username   = 'marcosviniciusjahu@gmail.com';
-    $mail->Password   = 'mgtq hdvv xzdi fugr';
+    $mail->Password   = '';
     $mail->SMTPSecure = 'tls';
     $mail->Port       = 587;
 
